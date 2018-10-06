6fd9f5d594186ba097234eea8e756de347ad7f8b
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-10-05 23:42:34 -0400

    At page 121
    Went over _Layout / _viewStart / ViewImports files and also basic razor expressions

--- Razor/Razor/Controllers/HomeController.cs
+++ Razor/Razor/Controllers/HomeController.cs
@@ -19,6 +19,9 @@ namespace Razor.Controllers
                 Category = "Watersports",
                 Price = 275m
             };
+            /// Sending more info to the view he recmmends using this for giving the view hints on how to display the data and not data that will be shown to the user
+            /// Accessed with @ViewBag.VariableName
+            ViewBag.StockLevel = 2; 
             return View(myProduct);
         }
     }
