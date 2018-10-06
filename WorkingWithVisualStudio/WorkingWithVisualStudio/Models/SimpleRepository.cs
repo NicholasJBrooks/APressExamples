780223e84977116e2d71f5387f4686a9c866a0b0
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-10-06 09:47:37 -0400

    Initial Commit for workingwithvisualstudio
    Creating the next project "WorkingWithVisualStudio" and making it a MVC project.

---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository SharedRepository => new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public SimpleRepository()
+        {
+            var InitialItems = new[]
+            {
+
+            };
+        }
+        
+    }
+}
