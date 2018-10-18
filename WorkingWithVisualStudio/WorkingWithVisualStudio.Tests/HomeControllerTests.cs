33d869dbc2862e07d7c982ed9a75fcf23466b227
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-10-17 22:18:33 -0400

    Added missing assemblies for testing project.
    Added the missing assemblies to the testing project through nuget.

---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+using System;
+using Moq;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModuleIsComplete()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            /// Assert
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+    }
+}
