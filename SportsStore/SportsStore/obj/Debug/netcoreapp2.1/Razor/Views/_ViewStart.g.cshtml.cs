87e4f18b91dceac28f7335c2f8910dc31c5185d7
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-10-22 19:59:29 -0400

    More Basic File structure and also added the start of Entity Framework

--- SportsStore/SportsStore/obj/Debug/netcoreapp2.1/Razor/Views/_ViewStart.g.cshtml.cs
+++ SportsStore/SportsStore/obj/Debug/netcoreapp2.1/Razor/Views/_ViewStart.g.cshtml.cs
@@ -14,12 +14,12 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
 #line 1 "C:\Users\NIck Brooks\Documents\GitHub\APressExamples\SportsStore\SportsStore\Views\_ViewImports.cshtml"
-using SportsStore.;
+using SportsStore.Models;
 
 #line default
 #line hidden
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"f368baf4b01e2a9d3a2c99327fa942dfc98dd257", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7e9d98212c457cc72388c2bdb8b97d31af536367", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
