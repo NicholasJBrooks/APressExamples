9981e769c68916f9466631f1eed080c785971dad
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-10-21 20:51:57 -0400

    Finished with the workingwithVisualStudios test section
    Ending on page 191

--- WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -10,26 +10,15 @@ namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
     {
-        public class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; set; }
-
-            public void AddProduct(Product product)
-            {
-                //do nothing not required for the test
-            }
-        }
 
         [Theory]
         [ClassData(typeof(ProductTestData))]
         public void IndexActionModuleIsComplete(Product[] products)
         {
             // Arrange
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository
-            {
-                Products = products
-            };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(x => x.Products).Returns(products); 
+            var controller = new HomeController() { Repository = mock.Object};
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
@@ -69,36 +58,21 @@ namespace WorkingWithVisualStudio.Tests
             Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
 
-        class PropertyOnceFakeRepository : IRepository
-        {
-            public int PropertyCounter { get; set; } = 0;
 
-            public IEnumerable<Product> Products {
-                get
-                {
-                    PropertyCounter++;
-                        return new[] { new Product { Name = "p1", Price = 100 } };
-                }
-            }
-
-            public void AddProduct(Product product)
-            {
-                // Do nothing - not needed for the test
-            }
-        }
 
         [Fact]
         public void RepositoryPropertyCalledOnce()
         {
             //Arrange
-            var repo = new PropertyOnceFakeRepository();
-            var controller = new HomeController() { Repository = repo };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(x => x.Products).Returns(new[] {new Product{ Name = "p1", Price = 100 } });
+            var controller = new HomeController() { Repository = mock.Object };
 
             //Act
             var result = controller.Index();
 
             //Assert
-            Assert.Equal(1, repo.PropertyCounter);           
+            mock.VerifyGet(x => x.Products, Times.Once);          
         }
 
 
