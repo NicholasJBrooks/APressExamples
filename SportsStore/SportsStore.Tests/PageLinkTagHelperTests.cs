6bd00033d895af886f791e22ff34e337732ebd76
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-10-27 14:29:12 -0400

    Added a component to displaay a menu for category choice
    Added the category choice menu and also added 2 tests

--- SportsStore/SportsStore.Tests/PageLinkTagHelperTests.cs
+++ SportsStore/SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -1,18 +1,19 @@
-﻿using SportsStore.Models;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moq;
+using SportsStore.Infrastructure;
 using SportsStore.Models.ViewModels;
 using Xunit;
-using SportsStore.Infrastructure;
-using System.Collections.Generic;
 
 namespace SportsStore.Tests
 {
+
     public class PageLinkTagHelperTests
     {
+
         [Fact]
         public void Can_Generate_Page_Links()
         {
@@ -21,35 +22,42 @@ namespace SportsStore.Tests
             urlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
                 .Returns("Test/Page1")
                 .Returns("Test/Page2")
-                .Returns("Test/page3");
+                .Returns("Test/Page3");
 
             var urlHelperFactory = new Mock<IUrlHelperFactory>();
-            urlHelperFactory.Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>()))
-                .Returns(urlHelper.Object);
+            urlHelperFactory.Setup(f =>
+                    f.GetUrlHelper(It.IsAny<ActionContext>()))
+                        .Returns(urlHelper.Object);
 
-            PageLinkTagHelper helper = new PageLinkTagHelper(urlHelperFactory.Object)
-            {
-                PageModel = new PagingInfo
-                {
-                    CurrentPage = 2,
-                    TotalItems = 28,
-                    ItemsPerPage = 10
-                },
-            };
+            PageLinkTagHelper helper =
+                    new PageLinkTagHelper(urlHelperFactory.Object)
+                    {
+                        PageModel = new PagingInfo
+                        {
+                            CurrentPage = 2,
+                            TotalItems = 28,
+                            ItemsPerPage = 10
+                        },
+                        PageAction = "Test"
+                    };
 
-            TagHelperContext ctx = new TagHelperContext(new TagHelperAttributeList(), new Dictionary<object, object>(), "");
+            TagHelperContext ctx = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), "");
 
             var content = new Mock<TagHelperContent>();
-            TagHelperOutput output = new TagHelperOutput("div", new TagHelperAttributeList(), (cache, encoder) => Task.FromResult(content.Object));
+            TagHelperOutput output = new TagHelperOutput("div",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
 
             // Act
             helper.Process(ctx, output);
 
             // Assert
             Assert.Equal(@"<a href=""Test/Page1"">1</a>"
-              + @"<a href=""Test/Page2"">2</a>"
-              + @"<a href=""Test/Page3"">3</a>",
-               output.Content.GetContent());
+                + @"<a href=""Test/Page2"">2</a>"
+                + @"<a href=""Test/Page3"">3</a>",
+                 output.Content.GetContent());
         }
     }
 }
--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -34,18 +34,29 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(configureRoutes => {
+            app.UseMvc(configureRoutes =>
+            {
                 configureRoutes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page{productPage}",
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
                     defaults: new { controller = "Product", action = "List" });
-
                 configureRoutes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
-                });
+                    name: null,
+                    template: "Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                configureRoutes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                configureRoutes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                configureRoutes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
 
+            });
             SeedData.EnsurePopulated(app);
         }
+            
     }
 }
