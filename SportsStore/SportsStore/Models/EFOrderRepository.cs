71e9e5c751ea024bc423fee6ab844d86fbb1cdff
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-11-07 23:03:31 -0500

    Adding EF structure for orders and creating the order repository and interface.

---
+++ SportsStore/SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFOrderRepository(ApplicationDbContext ctx)
+        {
+            context = ctx; 
+        }
+        public IQueryable<Order> orders => context.Orders
+            .Include(o => o.Lines)
+            .ThenInclude(l => l.Products);
+    }
+}
