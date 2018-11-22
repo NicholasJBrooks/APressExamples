4ab052f5e2742444566ca66e99b0d4a3b507fd0f
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2018-11-21 23:06:33 -0500

    Setting up Project for next chapter

---
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View(new Dictionary<string, string>{
+            ["Message"] = "This is the Index action"
+        });
+    }
+}
